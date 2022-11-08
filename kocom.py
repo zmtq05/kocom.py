cb9b6218ffed2496af10dc7a2c11fbff298a36ae
vifrost <67081596+vifrost@users.noreply.github.com>
2022-11-08 15:53:47 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -648,8 +648,7 @@ def publish_discovery(dev, sub=''):
             payload = {
                 'name': 'Kocom {} Light{}'.format(sub, num),
                 'cmd_t': 'kocom/{}/light/{}/command'.format(sub, num),
-#               'stat_t': 'kocom/{}/light/state'.format(sub),                      # 20221108 주석처리
-                'stat_t': 'kocom/{}-{}/light/state'.format(sub, num),              # 20221108 수정
+                'stat_t': 'kocom/{}/light/state'.format(sub),
                 'stat_val_tpl': '{{ value_json.light_' + str(num) + ' }}',
                 'pl_on': 'on',
                 'pl_off': 'off',
